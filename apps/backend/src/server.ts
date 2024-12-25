104cdf08b2cd3d583f56eddaca60354e9062f28f
Ayush Sahay <sahayayush385@gmail.com>
2024-12-26 00:31:19 +0530

    backend setup done

---
+++ apps/backend/src/server.ts
@@ -0,0 +1,8 @@
+console.log("Hello via Bun!");
+import app from "./app";
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
