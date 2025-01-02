1f7b54e366c22bf4aca0b492ce65722e38fd9487
Ayush Sahay <sahayayush385@gmail.com>
2025-01-02 10:54:19 +0530

    attempt route added

--- apps/backend/src/modules/routes.ts
+++ apps/backend/src/modules/routes.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import assignmentRoutes from "./assignments/assignmentRoutes";
 import templatesRoutes from "./templates/templatesRoutes";
+import attemptsRoutes from "./attempts/attemptsRoutes";
 import { templates, templatesRelations } from "../db/schema";
 
 const router = express.Router();
 
 router.use("/assignments", assignmentRoutes);
 router.use("/templates", templatesRoutes);
+router.use("/attempts", attemptsRoutes);
 
 export default router;
